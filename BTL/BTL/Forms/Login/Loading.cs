7bc77051e5729e3ca6aa78fc402ecfb85a9541b7
Nguyenvankhai1122000 <81398499+Nguyenvankhai1122000@users.noreply.github.com>
2021-12-02 09:35:20 +0700

    finish prohect

---
+++ BTL/BTL/Forms/Login/Loading.cs
@@ -0,0 +1,39 @@
+﻿using BTL.Forms.Main;
+using BTL.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTL.Forms.Login
+{
+    public partial class Loading : Form
+    {
+        public TaiKhoan user;
+
+        public Loading(TaiKhoan x)
+        {
+            InitializeComponent();
+            user = x;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            progressBar1.Increment(20);
+            if (progressBar1.Value == 500)
+            {
+                timer1.Enabled = false;
+                MainForm main = new MainForm(user);
+                this.Close();
+                main.Show();
+                
+            }
+        }
+    }
+}
---
+++ BTL/BTL/Models/KhachHang.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BTL.Models
+{
+    public partial class KhachHang
+    {
+        public KhachHang()
+        {
+            HoaDons = new HashSet<HoaDon>();
+        }
+
+        public string MaKh { get; set; }
+        public string TenKh { get; set; }
+        public string Sdt { get; set; }
+        public int? TichDiem { get; set; }
+
+        public virtual ICollection<HoaDon> HoaDons { get; set; }
+    }
+}
